1c644307a9c676d803b2aefde6e3005b982f813e
ChrisArce88 <chris.arce882@gmail.com>
2026-04-21 10:41:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const params = new URLSearchParams(window.location.search);
+const loc = params.get("loc") || "test";
 
-const partner = params.get("partner") || "unknown";
-const loc = params.get("loc") || "unknown";
+const iframe = document.createElement("iframe");
+iframe.src =
+  "https://forms.zohopublic.com/aldobettoni/form/MultiLocationFormTEST/formperma/FzWNb1lmOhqpaKpZGn35vKY4Xk-iFLnUczhBIjXRHTU"
+  + `?location_id=${loc}`;
 
-const formUrl =
-  `https://forms.zoho.com/TU_FORM` +
-  `?partner_id=${partner}` +
-  `&location_id=${loc}`;
+iframe.style = "height:500px;width:99%;border:none;";
+
+document.body.appendChild(iframe);
