5d5956a9b2aa3d48d1b01b4cf5e256fd581eb77b
ChrisArce88 <chris.arce882@gmail.com>
2026-04-21 11:28:40 -0700

    Update script to redirect instead of using iframe
    Redirect to the form URL with query parameters.

--- script.js
+++ script.js
@@ -6,14 +6,7 @@ const partner = params.get("partner") || "";
 const formBase =
   "https://forms.zohopublic.com/aldobettoni/form/MultiLocationFormTEST/formperma/FzWNb1lmOhqpaKpZGn35vKY4Xk-iFLnUczhBIjXRHTU";
 
-const iframe = document.createElement("iframe");
-
-iframe.src =
+window.location.href =
   formBase +
   "?partner=" + encodeURIComponent(partner) +
-  "&location_id=" + encodeURIComponent(location);
-
-iframe.style = "height:500px;width:99%;border:none;";
-
-document.getElementById("form-container").innerHTML = "";
-document.getElementById("form-container").appendChild(iframe);
+  "&location=" + encodeURIComponent(loc);
